a0d9e6ef289da555076d96c8b68593b6e424989a
Rafal Galka <rafal.galka@modernweb.pl>
2014-04-06 17:08:52 +0200

    sftp-sync
    Added non-interactive auth with given password.

--- sftp-sync.sh
+++ sftp-sync.sh
@@ -6,9 +6,13 @@
 #   all new or modified files are uploaded to the remote system. Uses
 #   a timestamp file ingeniously called .timestamp to keep track.
 
+ARGS=$(getopt -l 'password' -o 'p:' -- "$@" 2> /dev/null)
+eval set -- "$ARGS"
+
 timestamp=".timestamp"
 tempfile="/tmp/sftpsync.$$"
 count=0
+password=
 
 trap "/bin/rm -f $tempfile" 0 1 15      # zap tempfile on exit &sigs
 
@@ -16,6 +20,20 @@ if [ $# -eq 0 ] ; then
   echo "Usage: $0 user@host { remotedir }" >&2
   exit 1
 fi
+
+while true
+do
+    case "$1" in
+    --password | -p)
+        password=$2
+        shift;;
+    --)
+        shift
+        break
+        ;;
+    esac
+    shift
+done
 
 user="$(echo $1 | cut -d@ -f1)"
 server="$(echo $1 | cut -d@ -f2)"
@@ -50,7 +68,15 @@ echo "quit" >> $tempfile
 
 echo "Synchronizing: Found $count files in local folder to upload."
 
-if ! sftp -b $tempfile "$user@$server" ; then
+cmd="-b $tempfile $user@$server"
+if [ ! -z "$password" ]; then
+  export SSHPASS=$password
+  cmd="sshpass -e sftp -oStrictHostKeyChecking=no -oPubkeyAuthentication=no -oBatchMode=no $cmd"
+else
+  cmd="sftp $cmd"
+fi
+
+if ! `$cmd` ; then
   echo "Done. All files synchronized up with $server"
   touch $timestamp
 fi
