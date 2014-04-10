9b56c3dea9753d6e36501e09b8100c731883752b
Rafal Galka <rafal.galka@modernweb.pl>
2014-04-10 18:54:37 +0200

    sync-sftp merged into sync-ftp with -s|--ssh option

--- sync-ftp.sh
+++ sync-ftp.sh
@@ -1,19 +1,27 @@
 #!/bin/sh
 
-# sync-ftp - Sync files from current directory to FTP using lftp
+# sync-ftp - Sync files from current directory to FTP/SFTP using lftp
 
 if [ $# -eq 0 ] ; then
-  echo "Usage: $0 [-p|--password password] user@host [remotedir]" >&2
+  echo "Usage: $0 [options] user@host [remotedir]\n" >&2
+  echo "Options:" >&2
+  echo "         -s, --ssh" >&2
+  echo "             Use FTP over SSH (sftp://)\n" >&2
+  echo "         -p, --password password" >&2
+  echo "             Set user password\n" >&2
+  echo "         -e, --exclude local/path" >&2
+  echo "              Exclude local path, can be used multiple times" >&2
   exit 1
 fi
 
-ARGS=$(getopt -l 'password' -o 'p:' -- "$@" 2> /dev/null)
+ARGS=$(getopt -l 'password,exclude,ssh' -o 'p:,e:,s' -- "$@" 2> /dev/null)
 eval set -- "$ARGS"
 
 sourcedir=`pwd`
 remotedir="/"
-exclude="" # todo - add to options
+exclude=""
 
+ssh=0
 user=
 host=
 password=
@@ -22,7 +30,13 @@ while true
 do
     case "$1" in
     --password | -p)
-        password=":$2"
+        password="$2"
+        shift;;
+    --exclude | -e)
+        exclude="$exclude --exclude $2"
+        shift;;
+    --ssh | -s)
+        ssh=1
         shift;;
     --)
         shift
@@ -39,14 +53,20 @@ if [ ! -z "$2" ]; then
     remotedir=$2
 fi
 
-lftp -c "set ftp:list-options -a;
-open ftp://${user}${password}@${host};
+if [ "$ssh" -eq 1 ]; then
+  cmd="open -u ${user},${password} sftp://${host};"
+else
+  cmd="set ftp:list-options -a;
+       open ftp://${user}:${password}@${host};"
+fi
+
+lftp -c "$cmd
 lcd $sourcedir;
 cd $remotedir;
 mirror --reverse \
        --delete \
        --verbose \
-       --ignore-time"
-       # todo --exclude-glob local/path"
+       --ignore-time \
+       $exclude"
 
 exit 0
